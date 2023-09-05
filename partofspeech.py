fe5d943f19f02b30be92cbd49d8d74944a256645
tanmay0394 <104025894+tanmay0394@users.noreply.github.com>
2023-09-05 11:46:03 +0530

    Add files via upload

---
+++ partofspeech.py
@@ -0,0 +1,17 @@
+import spacy
+nlp = spacy.load("en_core_web_sm")
+about_text = (
+     "Gus Proto is a Python developer currently"
+     " working for a London-based Fintech"
+     " company. He is interested in learning"
+     " Natural Language Processing."
+ )
+about_doc = nlp(about_text)
+for token in about_doc:
+     print(
+         f"""
+ TOKEN: {str(token)}
+ 
+ TAG: {str(token.tag_):10} POS: {token.pos_}
+ EXPLANATION: {spacy.explain(token.tag_)}"""
+    )
